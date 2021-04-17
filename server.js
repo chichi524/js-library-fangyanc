cf58801e8520b30d0700871055f9989cb0501349
Yanchi Fang <yanchifang@YanchideMacBook-Pro.local>
2021-04-17 12:21:01 -0400

    over

--- server.js
+++ server.js
@@ -5,12 +5,7 @@ log('Express server')
 
 const express = require('express')
 const app = express();
-var ejs = require("ejs");
 const path = require('path');
-
-// Setting up a static directory for the files in /pub
-// using Express middleware.
-// Don't put anything in /pub that you don't want the public to have access to!
 
 app.use(express.static(path.join(__dirname, '/pub')))
 
@@ -20,7 +15,6 @@ app.use(express.static(path.join(__dirname, '/pub')))
 const port = process.env.PORT || 5000
 app.listen(port, () => {
 	log(`Listening on port ${port}...`)
-})  // localhost development port 5000  (http://localhost:5000)
-   // We've bound that port to localhost to go to our express server.
-   // Must restart web server when you make changes to route handlers.
+}) 
+
 
